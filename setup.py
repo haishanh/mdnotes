bf8b1929f62ce9bf9858b527ea5ca3146e3d3072
haishanh <haishanhan@gmail.com>
2015-10-12 19:05:21 +0800

    Add PyYAML as prerequsite in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     author='HAN Haishan',
     author_email='haishanhan@gmail.com',
     install_requires=[
-        'markdown>=2.6.2'
+        'markdown>=2.6.2',
+        'PyYAML>=3.11'
     ]
 )
