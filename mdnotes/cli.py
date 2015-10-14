6ffc1728b996ceb776b91edd9320b5a8a8eab762
haishanh <haishanhan@gmail.com>
2015-10-14 15:52:27 +0800

    Add argparse for arguments parsing

--- mdnotes/cli.py
+++ mdnotes/cli.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import argparse
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
 from mdnotes.config import Config
@@ -8,7 +9,15 @@ from mdnotes.note import Note
 from mdnotes.index import Index
 from mdnotes.utils import move_res
 
-def main():
+def parse_arguments():
+    description = 'mdnotes - turn your markdown files into\n' \
+                  'beautiful well structured html web page'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-b', '--build', action='store_true',
+                        help='build notes')
+    return vars(parser.parse_args())
+
+def build():
     config = Config()
     config, env = config.load()
     print('current dir is: ' + os.getcwd())
@@ -24,6 +33,11 @@ def main():
     index = Index(config)
     index.render(env, notes)
     move_res(config['theme_dir'] + '/resources', config['output_dir'])
+
+def main():
+    args = parse_arguments()
+    if args['build']:
+        build()
 
 if __name__ == '__main__':
     main()
