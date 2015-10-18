5f6042b40df046404a48e3f582085cdfbc76f837
haishanh <haishanhan@gmail.com>
2015-10-18 23:54:53 +1100

    Add TOC post process
    Now only 3 levels of heading wil be kept

--- mdnotes/note.py
+++ mdnotes/note.py
@@ -8,6 +8,47 @@ from mdnotes.utils import save_file, load_file, \
                           ensure_path, prt_exit
 from mdnotes.tag import Tag
 
+
+def process_toc(toc):
+    """
+    This will keep 3 levels of headings
+
+    In the example below, "heading5" will be removed
+
+    ## heading2
+    ### heading3
+    #### heading4
+    ##### heading5
+    """
+    depth = 3
+    stack = []
+    to_remove = []
+    index = 0
+    while True:
+        index = toc.find('<', index)
+        if index < 0:
+            break
+        if toc[index: index+3] == '<ul':
+            if len(stack) == depth:
+                to_remove.append(index)
+            stack.append(index)
+        if toc[index: index+4] == '</ul':
+            # len('</ul>')
+            index += 5
+            stack.pop()
+            if len(stack) == depth:
+                to_remove.append(index)
+        index += 1
+    to_remove.insert(0, 0)
+    to_remove.append(len(toc))
+    retstr = ''
+    index = 0
+    while index < len(to_remove):
+        retstr += toc[to_remove[index]:to_remove[index+1]]
+        index += 2
+    return retstr
+
+
 class Note(object):
 
     def __init__(self, filename, config, global_tags):
@@ -15,16 +56,16 @@ class Note(object):
         #      the filename is non ascii?
         bname = os.path.basename(filename)
         name, ext = os.path.splitext(bname)
-        ## Public
+        # Public
         self.name = name
         self.filename = filename
         self.title = ''
         self.category = ''
         self.article = ''
         self.toc = ''
-        self.tags = [] 
+        self.tags = []
         self.link = ''
-        ## Private
+        # Private
         self._global_tags = global_tags
         self._config = config
 
@@ -69,8 +110,8 @@ class Note(object):
         assert self._raw_content
         raw_content = self._raw_content
 
-        tridash_re=re.compile('^-{3,5}\s*$', re.MULTILINE)
-        m =  tridash_re.search(raw_content)
+        tridash_re = re.compile('^-{3,5}\s*$', re.MULTILINE)
+        m = tridash_re.search(raw_content)
         if m:
             start, end = m.span()
             # start is the 1st dash index
@@ -96,20 +137,22 @@ class Note(object):
         Return rendered html
         """
         # https://pythonhosted.org/Markdown/extensions/index.html
-        extensions=['extra', 'codehilite', 'admonition',
-                    'toc', 'smarty', 'sane_lists', 'wikilinks']
+        extensions = ['extra', 'codehilite', 'admonition',
+                      'toc', 'smarty', 'sane_lists', 'wikilinks']
         # TODO
-        extension_configs = {'toc' : {
+        extension_configs = {'toc': {
                                     'anchorlink': True,
                                     'permalink': True
                                 }
                             }
         output_format = 'html5'
         md = markdown.Markdown(extensions=extensions,
-                               extension_configs = extension_configs,
+                               extension_configs=extension_configs,
                                output_format=output_format)
         html = md.convert(self.md)
         toc = getattr(md, 'toc', '')
+        if toc:
+            toc = process_toc(toc)
         return html, toc
 
     def set_link(self):
